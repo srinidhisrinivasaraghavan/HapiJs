919a19b350ec04ba6e72c13519f0bcbddb277e1d
Srinidhi Raghavan <sraghavan@hqcorp.audiblemagic.com>
2016-08-04 17:57:03 -0700

    CRUD Implemented

---
+++ Basic CRUD/server.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const Hapi = require('hapi');
+const mongojs = require('mongojs');
+
+var booksPlugin = require('./routes/books');
+
+// Create a server with a host and port
+const server = new Hapi.Server();  
+server.connection({  
+    port: 3000
+});
+
+//Connect to database
+var databaseUrl = "mongodb://databaseuser:databasepassword@ds019480.mlab.com:19480/databaseall"; // 
+var collections = ["books"]
+server.app.db= mongojs(databaseUrl, collections);
+
+//Load plugins and start server
+server.register([booksPlugin], (err) => {
+  if (err) {
+    throw err;
+  }
+
+// Start the server
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Server running at:', server.info.uri);
+});
+
+});
